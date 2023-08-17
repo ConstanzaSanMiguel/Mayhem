1a2ab9c8bb8b2de1956ea67d9afad9a2e7288c2d
Constanza San Miguel <csanmiguel49@gmail.com>
2023-08-17 15:17:13 -0300

    Added array

--- js/app.js
+++ js/app.js
@@ -1,55 +1,61 @@
-//Inicio
-alert("Welcome to Mayhem shop! We hope you find what you came looking for!")
+const productos = [
+    { id: 1, producto: "Group shirt", characteristic: "Black", precioSinIVA: 4500, IVA: 25, disponible: true },
+    { id: 2, producto: "Member shirt", characteristic: "Black", precioSinIVA: 4500, IVA: 25, disponible: true },
+    { id: 3, producto: "Lightstick", characteristic: "", precioSinIVA: 6500, IVA: 30, disponible: true },
+    { id: 5, producto: "Necklace", characteristic: "Gold", precioSinIVA: 1500, IVA: 10, disponible: true },
+    { id: 6, producto: "Find a Way album", characteristic: "Random version", precioSinIVA: 2700, IVA: 15, disponible: true },
+    { id: 7, producto: "Be the change album", characteristic: "Version 1", precioSinIVA: 3000, IVA: 15, disponible: true },
+    { id: 8, producto: "Be the change album", characteristic: "Version 2", precioSinIVA: 3000, IVA: 15, disponible: true },
+    { id: 9, producto: "Black out album", characteristic: "Random version", precioSinIVA: 2500, IVA: 15, disponible: true },
+    { id: 10, producto: "Can't take us alive album", characteristic: "Only version", precioSinIVA: 2500, IVA: 15, disponible: true },
+];
 
 function precioTotal(precioProducto, IVA) {
     const precioConIVA = precioProducto * (1 + IVA / 100);
     return precioConIVA;
 }
 
-const productoElegido = prompt("What are you interested in? ('Clothes', 'merch' or 'album'):").toLowerCase();
+//Inicio
+alert("Welcome to Mayhem shop! We hope you find what you came looking for!")
+alert("Please choose the product you want to purchase.")
+productos.forEach(producto => {
+    console.log(`ID: ${producto.id}, Product: ${producto.producto} ${producto.characteristic}`);
+});
 
-let precioSinIVA;
-let precioConIVA;
+const userInput = prompt("Please type the ID of the product you'd like to purchase:");
 
-switch (productoElegido) {
-    case 'clothes':
-        precioSinIVA = 4500;
-        precioConIVA = precioTotal(precioSinIVA, 25);
-        break;
-    case 'merch':
-        precioSinIVA = 1500;
-        precioConIVA = precioTotal(precioSinIVA, 10);
-        break;
-    case 'album':
-        precioSinIVA = 2500;
-        precioConIVA = precioTotal(precioSinIVA, 15);
-        break;
-    default:
-        alert('Unrecognized product, please try again.');
-        break;
+if (userInput !== null) {
+    const productoElegidoID = parseInt(userInput);
+    const productoElegido = productos.find(producto => producto.id === productoElegidoID);
+
+    if (productoElegido) {
+        const precioConIVA = precioTotal(productoElegido.precioSinIVA, productoElegido.IVA);
+        console.log(`Product: ${productoElegido.producto}, ${productoElegido.characteristic}`);
+        console.log(`The total for ${productoElegido.producto} is $${precioConIVA.toFixed(2)}`);
+
+        // Preguntar datos para mandar mail de confirmación
+        alert(`If you want to continue with the purchase of ${productoElegido.producto} at a total of $${precioConIVA.toFixed(2)} please fill the following inputs:`)
+        let nombre = prompt("Please enter your full name:");
+        let direccion = prompt("Please enter you address:");
+        let email
+
+        // Validación del email
+        do {
+            email = prompt("Please enter your email:");
+            if (!email.includes('@')) {
+                alert("Invalid email. Please enter a valid email.");
+            }
+        } while (!email.includes('@'));
+
+        // Envío de datos
+        alert(`Thank you ${nombre}! An email confirmation was sent to ${email} to confirm the purchase.`);
+        console.log(`Data`)
+        console.log(`Name: ${nombre}`);
+        console.log(`Address: ${direccion}`);
+        console.log(`Email: ${email}`);
+    } else {
+        alert("Product not found.");
+    }
+} else {
+    alert("Wrong input. Please try again.");
 }
-
-if (precioConIVA) {
-    alert(`The total for ${productoElegido} is $${precioConIVA}`);
-    alert("To proceed with the purchase please fill the following fields:")
-
-    // Preguntar datos para mandar mail de confirmación
-    let nombre = prompt("Please enter your full name:");
-    let direccion = prompt("Please enter you address:");
-    let email
-     // Validación del email
-    do {
-        email = prompt("Please enter your email:");
-        if (!email.includes('@')) {
-            alert("Invalid email. Please enter a valid email.");
-        }
-    } while (!email.includes('@'));
-
-    // Envío de datos
-    alert(`Thank you ${nombre}! An email confirmation was sent to ${email} to confirm the purchase.`);
-    console.log(`Data`)
-    console.log(`Name: ${nombre}`);
-    console.log(`Address: ${direccion}`);
-    console.log(`Email: ${email}`);
-}
-
