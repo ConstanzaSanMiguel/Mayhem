a32edb651d6827136c5bd0755b984e71681d7a99
Constanza San Miguel <csanmiguel49@gmail.com>
2023-09-06 18:06:23 -0300

    carrito de compras

--- js/app.js
+++ js/app.js
@@ -1,78 +1,185 @@
-const productos = [
-    { id: 1, producto: "Group shirt", characteristic: "Black", precioSinIVA: 4500, IVA: 25, disponible: true },
-    { id: 2, producto: "Member shirt", characteristic: "Black", precioSinIVA: 4500, IVA: 25, disponible: true },
-    { id: 3, producto: "Lightstick", characteristic: "", precioSinIVA: 6500, IVA: 30, disponible: true },
-    { id: 4, producto: "Book", characteristic: "", precioSinIVA: 5155, IVA: 30, disponible: false },
-    { id: 5, producto: "Necklace", characteristic: "Gold", precioSinIVA: 1500, IVA: 10, disponible: true },
-    { id: 6, producto: "Find a Way album", characteristic: "Random version", precioSinIVA: 2700, IVA: 15, disponible: true },
-    { id: 7, producto: "Be the change album", characteristic: "Version 1", precioSinIVA: 3000, IVA: 15, disponible: true },
-    { id: 8, producto: "Be the change album", characteristic: "Version 2", precioSinIVA: 3000, IVA: 15, disponible: true },
-    { id: 9, producto: "Black out album", characteristic: "Random version", precioSinIVA: 2500, IVA: 15, disponible: true },
-    { id: 10, producto: "Can't take us alive album", characteristic: "Only version", precioSinIVA: 2500, IVA: 15, disponible: true },
-];
+let contenedorProductos = document.getElementById('misProductos');
 
-function precioTotal(precioProducto, IVA) {
-    const precioConIVA = precioProducto * (1 + IVA / 100);
-    return precioConIVA;
+function renderizarProductos(listaProductos) {
+    for (const prod of listaProductos) {
+        contenedorProductos.innerHTML += `
+            <div class="product-grid">
+                <div class="card">        
+                        <img src=${prod.foto} alt="" class="card_img">
+                        <div class="card_content">
+                            <h2 class="card_title">${prod.producto}</h2>
+                            <p class="card_price">$${prod.precio}</p>
+                            <form action="#">
+                            <label for="size">Choose the color & size:</label>
+                                <select class="card_options" name="color" id="color">
+                                    <option value="black">Black</option>
+                                    <option value="white">White</option>
+                                </select>
+                                <select class="card_options" name="size" id="size">
+                                    <option value="XS">XS</option>
+                                    <option value="S">S</option>
+                                    <option value="M">M</option>
+                                    <option value="L">L</option>
+                                    <option value="XL">XL</option>
+                                    <option value="XXL">XXL</option>
+                                </select>
+                                <input type="submit" value="${prod.disponible ? 'Add to cart' : 'OUT OF STOCK'}" id="addToCartBtn" class="compra btn btn-primary ${prod.disponible ? '' : 'not-available'}" data-product-id="${prod.id}" onclick="${prod.disponible ? 'agregarACarrito' : ''}(${prod.id})" ${prod.disponible ? '' : 'disabled'}>
+                                </form>
+                        </div>
+                </div>
+            </div>
+        `;
+    }
 }
 
-function isProductAvailable(productID) {
-    const product = productos.find(producto => producto.id === productID);
-    return product && product.disponible;
-}
+// Obtener el modal y el botón para abrirlo
+const modal = document.getElementById('carritoModal');
+const abrirCarritoBtn = document.getElementById('abrirCarrito');
+const cerrarCarritoBtn = document.getElementById('cerrarCarrito');
 
-const today = new Date();
-const timeOfPurchase = `${today.toLocaleDateString()}, ${today.getHours().toString().padStart(2, '0')}:${today.getMinutes().toString().padStart(2, '0')}`;
-
-//Inicio
-alert("Welcome to Mayhem shop! We hope you find what you came looking for!")
-alert("Please choose the product you want to purchase.")
-productos.forEach(producto => {
-    console.log(`ID: ${producto.id}, Product: ${producto.producto} ${producto.characteristic}`);
+// Función para abrir el modal del carrito
+abrirCarritoBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
 });
 
-const userInput = prompt("Please type the ID of the product you'd like to purchase:");
+// Función para cerrar el modal del carrito
+cerrarCarritoBtn.addEventListener('click', () => {
+    modal.style.display = 'none';
+});
 
-if (userInput !== null) {
-    const productoElegidoID = parseInt(userInput);
-    
-    if (isProductAvailable(productoElegidoID)) {
-        const productoElegido = productos.find(producto => producto.id === productoElegidoID);
-        const indexID = productos.indexOf(productoElegido);
-
-        const precioConIVA = precioTotal(productoElegido.precioSinIVA, productoElegido.IVA);
-        console.log(`Index: ${indexID}, Product: ${productoElegido.producto}, ${productoElegido.characteristic}`);
-        console.log(`The total for ${productoElegido.producto} is $${precioConIVA.toFixed(2)}`);
-
-        // Preguntar datos para mandar mail de confirmación
-        alert(`If you want to continue with the purchase of ${productoElegido.producto} at a total of $${precioConIVA.toFixed(2)} please fill the following inputs:`)
-        let nombre = prompt("Please enter your full name:");
-        let direccion = prompt("Please enter you address:");
-        let email
-
-        // Validación del email
-        do {
-            email = prompt("Please enter your email:");
-            if (!email.includes('@')) {
-                alert("Invalid email. Please enter a valid email.");
-            }
-        } while (!email.includes('@'));
-
-         // Envío de datos
-        alert(`Thank you ${nombre}! An email confirmation was sent to ${email} to confirm the purchase.`);
-        console.log(`Data on ${timeOfPurchase}`);
-        console.log(`Name: ${nombre}`);
-        console.log(`Address: ${direccion}`);
-        console.log(`Email: ${email}`);
-    } else {
-        alert("Product not found.");
-        console.log("Product not found.");
-        
-        if (isProductAvailable(productoElegidoID) === false) {
-            console.log("There's no more stock of the product. Please contact the provider.");
-        }
+// Cierra el modal si el usuario hace clic fuera de él
+window.addEventListener('click', (event) => {
+    if (event.target === modal) {
+        modal.style.display = 'none';
     }
-} else {
-    alert("Wrong input. Please try again.");
+});
+
+renderizarProductos(productos);
+
+let carrito = [];
+
+function agregarACarrito(productoId) {
+    console.log(`Se llamó a agregarACarrito con productoId: ${productoId}`);
+    const prodACarro = productos.find((producto) => producto.id === productoId);
+    carrito.push(prodACarro);
+    console.log("Producto agregado al carrito:", prodACarro);
+
+    Toastify({
+        text: `${prodACarro.producto} added to the cart`,
+        gravity: "bottom",
+        avatar: `${prodACarro.foto}`,
+        style: {
+            background: "linear-gradient(to right, #332f2f, #762020)",
+        }
+    }).showToast();
+
+    actualizarCarrito();
+    event.preventDefault();
 }
 
+function actualizarCarrito() {
+    let carritoProductosContainer = document.getElementById('carritoProductos');
+    carritoProductosContainer.innerHTML = ''; // Limpiar el contenido del carrito
+
+    let total = 0; // Inicializar el total
+
+    // Agregar productos al carrito y calcular el total
+    carrito.forEach((productos) => {
+        carritoProductosContainer.innerHTML += `
+            <div class="producto-en-carrito">
+                <p>${productos.producto} - $${productos.precio}</p>
+                <button onclick="quitarDelCarrito(${productos.id})">Eliminar</button>
+            </div>
+        `;
+
+        total += productos.precio; // Sumar el precio de cada producto al total
+    });
+
+    // Mostrar el total actualizado en la página
+    document.getElementById('totalCarrito').textContent = `Total: $${total.toFixed(2)}`;
+
+    // Guardar el carrito en el localStorage
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+}
+
+function quitarDelCarrito(productoId) {
+    console.log(`Se llamó a quitarDelCarrito con productoId: ${productoId}`);
+    for (let i = 0; i < carrito.length; i++) {
+        if (carrito[i].id === productoId) {
+            carrito.splice(i, 1); // Elimina el producto en la posición i
+            break; // Sale del bucle una vez que se ha eliminado un producto
+        }
+    }
+    actualizarCarrito();
+}
+
+const totalCarritoElement = document.getElementById('totalCarrito');
+
+function calcularTotal() {
+    let total = 0;
+
+    for (const producto of carrito) {
+        total += producto.precio;
+    }
+
+    return total;
+}
+
+// Guardar carrito al local
+const carritoGuardado = localStorage.getItem('carrito');
+if (carritoGuardado) {
+    carrito = JSON.parse(carritoGuardado);
+    actualizarCarrito();
+}
+
+const pagarButton = document.getElementById('pagarButton');
+const formularioPago = document.getElementById('formularioPago');
+const cerrarFormulario = document.getElementById('cerrarFormulario');
+const totalAPagarSpan = document.getElementById('totalAPagar');
+const datosEnvioForm = document.getElementById('datosEnvioForm');
+
+// Agregar evento click al botón Pagar
+pagarButton.addEventListener('click', () => {
+    const total = calcularTotal();
+    totalAPagarSpan.textContent = `$${total.toFixed(2)}`;
+    formularioPago.style.display = 'block'; // Mostrar el formulario modal
+});
+
+// Agregar evento click para cerrar el formulario modal
+cerrarFormulario.addEventListener('click', () => {
+    formularioPago.style.display = 'none'; // Ocultar el formulario modal
+    carrito = [];
+    actualizarCarrito();
+});
+
+datosEnvioForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // Aquí puedes obtener los valores de los campos del formulario y realizar la acción de pago/envío
+    const nombre = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const country = document.getElementById('country').value;
+    const city = document.getElementById('city').value;
+    const zipcode = document.getElementById('zipcode').value;
+    const address = document.getElementById('address').value;
+
+    // Poner métodos de pago acá
+    // Enviar los datos a un servidor o procesarlos de la manera que desees
+
+    Swal.fire({
+        title: `Thanks for the purchase, ${nombre}!`,
+        text: `The information has been sent to ${email}.`,
+        imageUrl: '../images/band.jpg',
+        imageWidth: 400,
+        imageHeight: 300,
+        imageAlt: 'Mayhem band',
+        confirmButtonColor: '#762020',
+    })
+
+    // Vaciar el carrito
+    carrito = [];
+    actualizarCarrito();
+
+    // Cerrar el formulario modal
+    formularioPago.style.display = 'none';
+});
